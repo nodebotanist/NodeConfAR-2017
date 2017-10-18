326cbbff736862d893b8e28a9e6e4cabcf7328ab
Kas Perch <the@nodebotani.st>
2017-10-18 17:42:18 -0500

    Added dynamoDB call

--- functions/tessel-social-bot/handler.js
+++ functions/tessel-social-bot/handler.js
@@ -1,16 +1,31 @@
 'use strict';
 
+const AWS = require('aws-sdk')
+var docClient = new AWS.DynamoDB.DocumentClient();
+
 module.exports.githubEvent = (event, context, callback) => {
   let statusCode = 200
+  let message = 'event added'
 
   if(event.repository && event.repository.name){
-    console.log(event.repository.name)
+    docClient.put({
+      TableName: 'SocialEvents',
+      Item: {
+        "time": new Date(),
+        type: 'GitHub Push',
+        repo: event.repository.name
+      }
+    }, (err, data) => {
+      if(err){
+        message = err
+      }
+    })
   }
 
   const response = {
     statusCode,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
+      message,
       input: event,
     }),
   };
